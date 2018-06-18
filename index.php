a93143c9e194e65266e3b24cd0cdf8e388a7fd6a
Michael Savchuk <pixelrobin@hotmail.com>
2018-06-17 23:59:29 -0700

    Make new 'News' layout

--- index.php
+++ index.php
@@ -1,36 +1,23 @@
 <?php get_header(); ?>
+	<?php $post_iterator = 0; ?>
+
 	<?php if (have_posts()): while (have_posts()): the_post(); ?>
+		<?php
+			$thumb = has_post_thumbnail();
+			$single = is_singular();
+		?>
 
-		<section class="blog-post mb-4 p-0">
+		<section class="blog-post <?php echo $thumb ? 'blog-post--with-thumbnail' : ''; ?> <?php echo $single ? '' : 'blog-post--preview'; ?>">
 			<article>
-				<div class="blog-post__image">
-					<?php if (has_post_thumbnail()): ?>
-						<?php the_post_thumbnail('medium_large'); ?>
-						<div class="blog-post__image__cover"></div>
-					<?php endif; ?>
+				<?php if ($thumb): ?>
+					<div class="blog-post__thumbnail" style="background-image: url(<?php the_post_thumbnail_url($single ? 'large' : 'medium_large'); ?>);"></div>
+					<?php $post_iterator ++; ?>
+				<?php endif; ?>
 
-					<div class="blog-post__heading white">
-						<a href="<?php the_permalink(); ?>" class="link-inherit"><h1><?php the_title() ?></h1></a>
-						
-						<dl>
-							<dt>Posted on</dt>
-							<dd><?php the_date(); ?></dd>
-						</dl>
-					</div>
-				</div>
-
-				<div class="p-2">
-					
-					<?php if (is_singular()): ?>
-						<?php the_content(); ?>
-					<?php else: ?>
-						<?php the_excerpt(); ?>
-
-						<br>
-						<div class="text-centered">
-							<a href="<?php the_permalink(); ?>" class="button">Read more</a>
-						</div>
-					<?php endif; ?>
+				<div class="blog-post__content">
+					<h1><?php the_title(); ?></h1>
+					<?php the_excerpt(); ?>
+					<a href="<?php the_permalink(); ?>" class="button">Read More</a>
 				</div>
 			</article>
 		</section>
