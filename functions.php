b1f381659b825f86c9f0c990dd1391e785298d0d
Michael Savchuk <pixelrobin@hotmail.com>
2018-10-22 10:59:28 -0700

    Remove commented code from functions.php

--- functions.php
+++ functions.php
@@ -21,76 +21,6 @@ function sgc_theme_parse_events($posts, $from = null, $to = null) {
 		$post->sgc_theme_time_range = $single_day
 			? 'All Day'
 			: $start_date->format('g:H A') . ' &#8211; ' . $end_date->format('g:H A');
-		
-		/*$time         = '';
-		$date_weekday = $start_date->format('D');
-		$date_day     = $start_date->format('j');
-		$ends         = false;
-		$show_date    = true;
-		$tag          = null;
-		$description  = array();
-
-		if ($single_day) {
-			if ($post->all_day == '1') {
-				$time = "All Day";
-				$description = array(
-					array(
-						'name'  => 'Date:',
-						'value' => $start_date->format('F j, Y')
-					)
-				);
-			} else {
-				$time = $start_date->format('g:H A') . ' - ' . $end_date->format('g:H A');
-				$description = array(
-					array(
-						'name'  => 'Starts:',
-						'value' => $start_date->format('F j, Y \a\t g:i A')
-					),
-
-					array(
-						'name'  => 'Ends:',
-						'value' => 'Same day at ' . $end_date->format('g:i A')
-					)
-				);
-			}
-		} else {
-			if ($start_date > $from) {
-				$time = $start_date->format('g:H A') . ' (Ends ' . $end_date->format('n/j') . ')';
-			} else {
-				if (!is_null($to) && $end_date > $to) {
-					$time = 'Ends ' . $end_date->format('n/j') . ' at ' . $end_date->format('g:H A');
-					$show_date = false;
-				} else {
-					$time = 'Ends at ' . $end_date->format('g:H A');
-					$tag = 'Ends';
-				}
-			}
-
-			$description = array(
-				array(
-					'name'  => 'Starts:',
-					'value' => $start_date->format('F j, Y \a\t g:i A')
-				),
-
-				array(
-					'name'  => 'Ends:',
-					'value' => 'Same day at ' . $end_date->format('g:i A')
-				)
-			);
-		}
-
-		if ($start_date < $from) {
-			$date_weekday = $end_date->format('D');
-			$date_day     = $end_date->format('j');
-		}
-
-		$post->sgc_theme_time         = $time;
-		$post->sgc_theme_date_weekday = $date_weekday;
-		$post->sgc_theme_date_day     = $date_day;
-		$post->sgc_theme_show_date    = $show_date;
-		$post->sgc_theme_tag          = $tag;
-		$post->sgc_theme_description  = $description;
-		*/
 	}
 
 	return $posts;
@@ -111,18 +41,3 @@ function sgc_theme_date_from_params() {
 
 	return $date;
 }
-
-/*
-// http://blog.room34.com/archives/5360
-function sgc_theme_empty_content($str) {
-	return trim(str_replace('&nbsp;','',strip_tags($str,'<img>'))) == '';
-}
-
-function sgc_theme_remove_dead_nav_links($atts, $item, $args, $depth) {
-	if ($item->url == '#') $atts['href'] = '';
-
-	return $atts;
-}
-
-add_filter('nav_menu_link_attributes', sgc_theme_remove_dead_nav_links, 10, 4);
-*/
